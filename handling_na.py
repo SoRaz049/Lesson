c1b6b26d6ee4d18f66b9e80083e6fd69c94145a0
Swaraj Barnwal <ikku12piku@gmail.com>
2024-03-10 23:54:44 +0545

    Data cleaned and replaced the null values with the forward fill, mean and backward fill.

--- handling_na.py
+++ handling_na.py
@@ -1,13 +1,25 @@
+import numpy as np
 import pandas as pd
 
 
 df = pd.read_csv("pe.csv")
 
 
-df.fillna({
-    'eps': df.eps.mean(),
-    'pe' : df.pe.mean()
-},inplace= True)
+# df.fillna({
+#     'eps': df.eps.mean(),
+#     'pe' : df.pe.mean()
+# },inplace= True)
 
-df_droped = df.dropna()
-print(df_droped)
+# df_droped = df.dropna()
+# print(df_droped)
+
+df_clean = df.replace({
+    'eps': [-1, -999],
+    'revenue': -999,
+    'price' : 00
+},np.nan)
+
+
+df_clean.ffill(inplace = True)
+
+print(df_clean)
